4a5f754f52f138584eb6db52e39acfa0ed5cc75a
human-d3v <brytop05@gmail.com>
2025-02-05 13:10:34 -0700

    Added caller and ui

---
+++ lua/txt-files/caller.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+function M.MakeCall(word)
+	local cmd = '~/.local/share/nvim/lazy/txt-files.nvim/api-caller/dict-api ' .. word
+	local handle = io.popen(cmd,"w")
+	if handle == nil then
+		return error("error in making api call")
+	end
+	local result = handle:read("*a")
+	if result == nil then
+		return error("error in reading api call result")
+	end
+	handle:close()
+	return result 
+	-- returns a string representing an object 
+	--{'word'=<wd>, 'def'=[<def1>, <def2>, ...], 'syn'=[<syn1>, <syn2>, ...]}
+end
+
+function M.GetDefOrSyn(selection)
+	if selection ~= 'def' and selection ~= 'syn' then
+		return error("opt mst be 'def' or 'syn', you passed " .. selection)
+	end
+	local header = (selection == 'def') and 'Dictionary' or 'Thesaurus'
+	vim.cmd('normal! gv"xy')
+	local word = vim.fn.getreg('x')
+	word = string.gsub(word, '\n', "") --strip newline
+	local word_object_string = M.MakeCall(word) -- returns string
+	local table_func = load("return " .. word_object_string) 
+	if table_func == nil then
+		return error("error in building table object")
+	end
+	local word_object = table_func() -- returns table
+	
+	local ui = require('txt-files.ui')
+	ui.CreateFloatingWindow(word_object, header, selection)
+end
---
+++ lua/txt-files/ui.lua
@@ -0,0 +1,72 @@
+local M = {}
+
+function M.renderTitleSeparator(num)
+	local sep = ''
+	for i=1, num, 1 do
+		sep = sep .. "~"
+	end
+	return sep
+end
+
+function M.renderBody(obj, selection)
+	local definitions = obj.def
+	local synonyms = obj.syn
+	local lines = {}
+	if selection == 'def' then
+		for _, def in ipairs(definitions) do
+			local def_to_render = " - " .. def
+			table.insert(lines, def_to_render)
+		end
+	elseif selection == 'syn' then
+    for _, syn in ipairs(synonyms) do
+      local syn_to_render = " - " .. syn
+      table.insert(lines, syn_to_render)
+    end
+	end
+	return lines
+end
+
+function M.CreateFloatingWindow(word_object, header, selection)
+	local bufnr = vim.api.nvim_create_buf(false, true)
+	local window_width = vim.api.nvim_win_get_width(0)
+	local window_height = vim.api.nvim_win_get_height(0)
+	--calculate floating window size
+	local float_width = math.ceil(window_width * 0.4)
+	local float_height = math.ceil(window_height * 0.2)
+	--calculate starting position
+	local col = math.ceil((window_width - float_width) / 2)
+	local row = math.ceil((window_height - float_height) / 2)
+	--set opts
+	local ui_opts = {
+		relative = 'editor', 
+		width = float_width,
+		height = float_height,
+		col = col,
+		row = row,
+		style = 'minimal',
+		border= {
+		  {"╭", "FloatBorder"},
+      {"─", "FloatBorder"},
+      {"╮", "FloatBorder"},
+      {"│", "FloatBorder"},
+      {"╯", "FloatBorder"},
+      {"─", "FloatBorder"},
+      {"╰", "FloatBorder"},
+      {"│", "FloatBorder"}
+		},
+	}
+	vim.cmd("highlight FloatBorder guifg=white")
+	--create floating window
+	vim.api.nvim_open_win(bufnr, true, ui_opts)
+	-- create separator
+	local header_separator = M.renderTitleSeparator(float_width)
+	local body = M.renderBody(word_object, selection)
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, 
+		{header, header_separator, word_object.word, unpack(body)}
+	)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'q', '<cmd>bd!<CR>' {noremap=true, 
+		silent=true}
+	)
+end
+
+return M
