ddc137a762cf3bded90f4893da581e8d8f445fac
human-d3v <brytop05@gmail.com>
2025-02-12 10:25:05 -0700

    Fixed linebreak. It wasn't actually posting the line to the buf

--- lua/txt-files/funcs.lua
+++ lua/txt-files/funcs.lua
@@ -4,6 +4,7 @@ function M.LineBreak()
 	local line_num = vim.api.nvim_win_get_cursor(0)[1]
 	local win = vim.api.nvim_get_current_win()
 	local br = string.rep("~", 79) --color line value
+	vim.api.nvim_buf_set_lines(0, line_num, line_num, false, {br})
 	vim.api.nvim_win_set_cursor(win, {line_num + 1, 0})
 end
 
